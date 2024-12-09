fbd3e7b44ad11c3ae6cacb59ab7ff6d5c02c0bb9
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-12-09 10:01:35 -0400

    added utility functions and organized

--- src/lib/utils/encryption.ts
+++ src/lib/utils/encryption.ts
@@ -1,33 +1,10 @@
-import { TUser } from "@/db/schema";
+
 import argon2 from "argon2";
-import { randomBytes } from "crypto";
 
-const encoding: BufferEncoding = "base64" as const;
-export async function hashPassword(password: string, userSalt?: string): Promise<{
-  hashedPassword: string;
-  salt: string;
-}> {
-  const salt = userSalt ? stringToBuffer(userSalt) : randomBytes(16);
-  const hash = await argon2.hash(password, {
-    type: argon2.argon2id,
-    salt,
-    parallelism: 1,
-  });
-  return { hashedPassword: hash, salt: bufferToString(salt) };
+export async function hashPassword(password: string): Promise<string> {
+  return await argon2.hash(password);
 }
 
-export function bufferToString(buffer: Buffer): string {
-  
-  return buffer.toString(encoding);
-}
-
-export function stringToBuffer(str: string): Buffer {
-  return Buffer.from(str, encoding);
-}
-
-//TODO: has to be tested
-export async function isSamePassword(user: TUser, unverifiedPassword: string): Promise<boolean> {
-  return await argon2.verify(user.password, unverifiedPassword, {
-    secret: stringToBuffer(user.salt),
-  });
+export async function isSamePassword(userPassword:string, unverifiedPassword: string): Promise<boolean> {
+  return await argon2.verify(userPassword, unverifiedPassword);
 }
--- src/lib/utils/session.ts
+++ src/lib/utils/session.ts
@@ -1,25 +1,47 @@
-import { addSession } from "@/db/sessions";
+import { addSession, deleteSession, getSession } from "@/db/sessions";
 import { sha256 } from "@oslojs/crypto/sha2";
-import { encodeBase32LowerCaseNoPadding, encodeHexLowerCase } from "@oslojs/encoding";
+import {
+  encodeBase32LowerCaseNoPadding,
+  encodeHexLowerCase,
+} from "@oslojs/encoding";
 import { getOneWeekFromNow } from "@/lib/utils/general";
-import { createSessionCookie } from "./cookies";
+import { createSessionCookie, deleteSessionCookie, getSessionCookie } from "./cookies";
 
 function generateSessionToken(): string {
-	const bytes = new Uint8Array(20);
-	crypto.getRandomValues(bytes);
-	const token = encodeBase32LowerCaseNoPadding(bytes);
-	return token;
+  const bytes = new Uint8Array(20);
+  crypto.getRandomValues(bytes);
+  const token = encodeBase32LowerCaseNoPadding(bytes);
+  return token;
 }
-export function encodeSessionToken(token: string): string {
+export function generateSessionId(token: string): string {
   return encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
 }
 
-export async function executeSessionFlow(userId: number): Promise<void>{
+export async function executeSessionFlow(userId: number): Promise<void> {
   const token = generateSessionToken();
   const session = await addSession(
     userId,
-    encodeSessionToken(token),
+    generateSessionId(token),
     getOneWeekFromNow()
   );
   createSessionCookie(token, session.expiresAt);
+}
+
+export async function isValidSession(): Promise<boolean> {
+  const cookie = getSessionCookie();
+  const token = cookie?.value;
+  if (!token) return false;
+  const session = await getSession(generateSessionId(token));
+  if (!session) return false;
+  if (session.expiresAt < new Date(Date.now())) {
+    await deleteSession(session.id);
+    deleteSessionCookie();
+    return false;
+  }
+  return true;
+}
+
+export async function invalidateSession(sessionId: string): Promise<void> {
+  await deleteSession(sessionId);
+  deleteSessionCookie();
 }
